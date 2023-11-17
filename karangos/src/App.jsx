bd237c752e13195b5c573dbd82cbda0e658c4355
LeoSParra <108582759+LeoSParra@users.noreply.github.com>
2023-11-17 13:06:14 +0000

    (17/11) Consumo de dados de API remota

--- karangos/src/App.jsx
+++ karangos/src/App.jsx
@@ -4,34 +4,37 @@ import HeaderBar from './ui/HeaderBar'
 import { ThemeProvider } from '@mui/material/styles'
 import theme from './utils/theme'
 import Box from '@mui/material/Box'
-import { Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import CssBaseline from '@mui/material/CssBaseline'
 
 import Homepage from './pages/Homepage'
-import Carlist from './pages/CarList'
+import CarList from './pages/CarList'
 import CustomerList from './pages/CustomerList'
 
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <Box sx={{
-          width: '100vw',     // toda a largura da janela
-          minHeight: '100vh',  // no mínimo, toda a altura da janela
-          // cinza escuro, no modo dark
-          backgroundColor: 'background.default' 
-        }}>
-          <HeaderBar />
-          <Box sx={{ m: '25px' /* margem de 25px de todos lados */}}></Box>
-          <Routes>
-            <Route path="/" element={ <Homepage /> } />
-            <Route path="/cars" element={ <CarList/> } />
-            <Route path="/costumers" element ={ <CostumerList/> } />
-          </Routes>
-        </Box>
-      </ThemeProvider>
-    </BrowserRouter>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline /> { /* Reseta o CSS padrão e o adapta para o tema */ }
+          <Box sx={{
+            width: '100vw',     // toda a largura da janela
+            minHeight: '100vh',  // no mínimo, toda a altura da janela
+            // cinza escuro, no modo dark
+            backgroundColor: 'background.default' 
+          }}>
+            <HeaderBar />
+            <Box sx={{ m: '25px' /* Margem de 25px de todos os lados */ }}>
+              <Routes>
+                <Route path="/" element={ <Homepage /> } />
+                <Route path="/cars" element={ <CarList /> } />
+                <Route path="/customers" element={ <CustomerList /> } />
+              </Routes>
+            </Box>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </>
   )
 }
